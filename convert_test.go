aa476b1fc2c06a94e25ed28c21a45b85420adebf
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-23 23:43:15 +0900

    Implement conversion methods for Float types and add corresponding tests

---
+++ convert_test.go
@@ -0,0 +1,122 @@
+package floats
+
+import (
+	"math"
+	"testing"
+)
+
+func TestFloat32_Float32(t *testing.T) {
+	tests := []struct {
+		in   Float32
+		want Float32
+	}{
+		{
+			in:   1.0,
+			want: 1.0,
+		},
+		{
+			in:   2.0,
+			want: 2.0,
+		},
+	}
+
+	for _, tt := range tests {
+		if got := tt.in.Float32(); got != tt.want {
+			t.Errorf("Float32(%x).Float32() = %x, want %x", tt.in, got, tt.want)
+		}
+	}
+}
+
+func TestFloat32_Float64(t *testing.T) {
+	tests := []struct {
+		in   Float32
+		want Float64
+	}{
+		{
+			in:   1.0,
+			want: 1.0,
+		},
+		{
+			in:   2.0,
+			want: 2.0,
+		},
+		{
+			in:   math.MaxFloat32,
+			want: math.MaxFloat32,
+		},
+		{
+			in:   math.SmallestNonzeroFloat32,
+			want: math.SmallestNonzeroFloat32,
+		},
+	}
+
+	for _, tt := range tests {
+		if got := tt.in.Float64(); got != tt.want {
+			t.Errorf("Float32(%x).Float64() = %x, want %x", tt.in, got, tt.want)
+		}
+	}
+}
+
+func TestFloat64_Float32(t *testing.T) {
+	tests := []struct {
+		in   Float64
+		want Float32
+	}{
+		{
+			in:   1.0,
+			want: 1.0,
+		},
+		{
+			in:   2.0,
+			want: 2.0,
+		},
+		{
+			in:   math.MaxFloat32,
+			want: math.MaxFloat32,
+		},
+		{
+			in:   math.SmallestNonzeroFloat32,
+			want: math.SmallestNonzeroFloat32,
+		},
+
+		// overflow
+		{
+			in:   math.MaxFloat64,
+			want: Float32(math.Inf(1)),
+		},
+
+		// underflow
+		{
+			in:   math.SmallestNonzeroFloat64,
+			want: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		if got := tt.in.Float32(); got != tt.want {
+			t.Errorf("Float64(%x).Float32() = %x, want %x", tt.in, got, tt.want)
+		}
+	}
+}
+
+func TestFloat64_Float64(t *testing.T) {
+	tests := []struct {
+		in   Float64
+		want Float64
+	}{
+		{
+			in:   1.0,
+			want: 1.0,
+		},
+		{
+			in:   2.0,
+			want: 2.0,
+		},
+	}
+
+	for _, tt := range tests {
+		if got := tt.in.Float64(); got != tt.want {
+			t.Errorf("Float64(%x).Float64() = %x, want %x", tt.in, got, tt.want)
+		}
+	}
+}
