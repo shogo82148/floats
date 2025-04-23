d1ee4038742210f88b1253d91926d9b57d8d5f4b
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-24 00:47:53 +0900

    Add benchmarks for Float32 to Float64 and Float64 to Float32 conversions

--- convert_test.go
+++ convert_test.go
@@ -2,6 +2,7 @@ package floats
 
 import (
 	"math"
+	"runtime"
 	"testing"
 )
 
@@ -57,6 +58,13 @@ func TestFloat32_Float64(t *testing.T) {
 	}
 }
 
+func BenchmarkFloat32_Float64(b *testing.B) {
+	f := Float32(1.0)
+	for b.Loop() {
+		runtime.KeepAlive(f.Float64())
+	}
+}
+
 func TestFloat64_Float32(t *testing.T) {
 	tests := []struct {
 		in   Float64
@@ -96,6 +104,13 @@ func TestFloat64_Float32(t *testing.T) {
 		if got := tt.in.Float32(); got != tt.want {
 			t.Errorf("Float64(%x).Float32() = %x, want %x", tt.in, got, tt.want)
 		}
+	}
+}
+
+func BenchmarkFloat64_Float32(b *testing.B) {
+	f := Float64(1.0)
+	for b.Loop() {
+		runtime.KeepAlive(f.Float32())
 	}
 }
 
