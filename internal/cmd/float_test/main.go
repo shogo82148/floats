3271869adec4a55a93ccee0e2c782103cc057d01
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-24 01:35:57 +0900

    Implement f16_to_f32 conversion and add error handling

--- internal/cmd/float_test/main.go
+++ internal/cmd/float_test/main.go
@@ -29,6 +29,10 @@ func main() {
 	go showProgress()
 
 	switch os.Args[1] {
+	case "f16_to_f32":
+		if err := f16_to_f32(); err != nil {
+			log.Fatal(err)
+		}
 	case "f32_to_f64":
 		if err := f32_to_f64(); err != nil {
 			log.Fatal(err)
@@ -38,6 +42,37 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+}
+
+func f16_to_f32() error {
+	for {
+		var s16, s32, flag string
+		if _, err := fmt.Scanf("%s %s %s", &s16, &s32, &flag); err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return err
+		}
+
+		f16, err := parseFloat16(s16)
+		if err != nil {
+			return err
+		}
+
+		f32, err := parseFloat32(s32)
+		if err != nil {
+			return err
+		}
+
+		got := f16.Float32()
+		if cmp.Compare(got, f32) != 0 {
+			log.Printf("f16: %s, f32: %s", s16, s32)
+			log.Printf("got: %x, want: %x", got, f32)
+			return fmt.Errorf("f16(%x).Float32() = %x, want %x", f16, got, f32)
+		}
+		count.Add(1)
+	}
+	return nil
 }
 
 func f32_to_f64() error {
@@ -100,6 +135,14 @@ func f64_to_f32() error {
 		count.Add(1)
 	}
 	return nil
+}
+
+func parseFloat16(s string) (floats.Float16, error) {
+	bits, err := strconv.ParseUint(s, 16, 16)
+	if err != nil {
+		return 0, err
+	}
+	return floats.Float16(bits), nil
 }
 
 func parseFloat32(s string) (floats.Float32, error) {
