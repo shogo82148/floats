285b8c88186ee877ff2bbc824a5f3d53d98dc0f8
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-24 19:55:01 +0900

    Refactor TestFloat16_Float32 to use cmp for NaN comparison and update test cases

--- convert_test.go
+++ convert_test.go
@@ -1,6 +1,7 @@
 package floats
 
 import (
+	"cmp"
 	"math"
 	"runtime"
 	"testing"
@@ -13,20 +14,25 @@ func TestFloat16_Float32(t *testing.T) {
 	}{
 		// from https://en.wikipedia.org/wiki/Half-precision_floating-point_format
 		{0x0000, 0},
-		{0x0001, 0x1p-24},     // smallest positive subnormal number
-		{0x03ff, 0x1.ff8p-15}, // largest positive subnormal number
-		{0x0400, 0x1p-14},     // smallest positive normal number
-		{0x3555, 0x1.554p-02}, // nearest value to 1/3
-		{0x3bff, 0x1.ffcp-01}, // largest number less than one
-		{0x3c00, 0x1p+00},     // one
-		{0x3c01, 0x1.004p+00}, // smallest number larger than one
-		{0x7bff, 0x1.ffcp+15}, // largest normal number
+		{0x0001, 0x1p-24},              // smallest positive subnormal number
+		{0x03ff, 0x1.ff8p-15},          // largest positive subnormal number
+		{0x0400, 0x1p-14},              // smallest positive normal number
+		{0x3555, 0x1.554p-02},          // nearest value to 1/3
+		{0x3bff, 0x1.ffcp-01},          // largest number less than one
+		{0x3c00, 0x1p+00},              // one
+		{0x3c01, 0x1.004p+00},          // smallest number larger than one
+		{0x7bff, 0x1.ffcp+15},          // largest normal number
+		{0x7c00, Float32(math.Inf(1))}, // infinity
 		{0x8000, -0},
 		{0xc000, -2},
+		{0xfc00, Float32(math.Inf(-1))}, // negative infinity
+		{0x7e00, Float32(math.NaN())},   // NaN
 	}
 
 	for _, tt := range tests {
-		if got := tt.in.Float32(); got != tt.want {
+		// we use cmp.Compare because NaN != NaN
+		// and we want to check if they are equal.
+		if got := tt.in.Float32(); cmp.Compare(got, tt.want) != 0 {
 			t.Errorf("Float16(%x).Float32() = %x, want %x", tt.in, got, tt.want)
 		}
 	}
