ed8a0980475793a2d33caf595549606f9b1f2dca
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-26 23:32:52 +0900

    Implement IsInf method for Float128 to check for infinity values

--- float128.go
+++ float128.go
@@ -11,6 +11,11 @@ const (
 )
 
 var (
+	uvinf128    = ints.Uint128{0x7fff_0000_0000_0000, 0x0000_0000_0000_0000} // Infinity value for Float128
+	uvneginf128 = ints.Uint128{0xffff_0000_0000_0000, 0x0000_0000_0000_0000} // Negative Infinity value for Float128
+	// uvnan128    = ints.Uint128{0x7fff_8000_0000_0000, 0x0000_0000_0000_0000} // NaN value for Float128
+	// uvone128    = ints.Uint128{0x3fff_0000_0000_0000, 0x0000_0000_0000_0000} // One value for Float128
+
 	signMask128 = ints.Uint128{0x8000_0000_0000_0000, 0x0000_0000_0000_0000} // mask for sign bit
 	fracMask128 = ints.Uint128{0x0000_ffff_ffff_ffff, 0xffff_ffff_ffff_ffff}
 )
@@ -22,4 +27,13 @@ type Float128 ints.Uint128
 func (a Float128) IsNaN() bool {
 	return a[0]&(mask128<<(shift128-64)) == (mask128<<(shift128-64)) &&
 		!ints.Uint128(a).And(fracMask128).IsZero()
+}
+
+// IsInf reports whether a is an infinity, according to sign.
+// If sign > 0, IsInf reports whether a is positive infinity.
+// If sign < 0, IsInf reports whether a is negative infinity.
+// If sign == 0, IsInf reports whether a is either infinity.
+func (a Float128) IsInf(sign int) bool {
+	b := ints.Uint128(a)
+	return sign >= 0 && b == uvinf128 || sign <= 0 && b == uvneginf128
 }
