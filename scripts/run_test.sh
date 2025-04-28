69ae313b5bc7c935e561f9050eb581ddbe8cad27
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-28 20:10:12 +0900

    Add float conversion tests and update run_test.sh for conditional execution

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -9,4 +9,8 @@ TEST_NAME=$1
 ROOT=$(cd "$(dirname "$0")"; cd ..; pwd)
 cd "$ROOT"
 
-"$ROOT/bin/testfloat_gen" -level 2 -seed "$SEED" "$TEST_NAME" | go run ./internal/cmd/float_test "$TEST_NAME"
+if [[ $TEST_NAME =~ _to_[iu]64$ ]]; then
+  "$ROOT/bin/testfloat_gen" -level 2 -seed "$SEED" -rminMag "$TEST_NAME" | go run ./internal/cmd/float_test "$TEST_NAME"
+else
+  "$ROOT/bin/testfloat_gen" -level 2 -seed "$SEED" "$TEST_NAME" | go run ./internal/cmd/float_test "$TEST_NAME"
+fi
