4c8c212598a80920cea61dc8da1de39761dd6cc5
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-28 22:31:00 +0900

    Refactor run_test.sh to enhance test case handling for Float128 conversions

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -11,6 +11,8 @@ cd "$ROOT"
 
 if [[ $TEST_NAME =~ _to_[iu]64$ ]]; then
   "$ROOT/bin/testfloat_gen" -level 2 -seed "$SEED" -rminMag "$TEST_NAME" | go run ./internal/cmd/float_test "$TEST_NAME"
+elif [[ $TEST_NAME =~ ^f(16|32|64|128)_to_f(16|32|64|128)$ ]]; then
+  "$ROOT/bin/testfloat_gen" -level 2 -seed "$SEED" "$TEST_NAME" | go run ./internal/cmd/float_test "$TEST_NAME"
 else
   "$ROOT/bin/testfloat_gen" -seed "$SEED" "$TEST_NAME" | go run ./internal/cmd/float_test "$TEST_NAME"
 fi
