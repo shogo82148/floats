7ef6e85b3c93bc98302a8e8390f5b14f00b42270
ICHINOSE Shogo <shogo82148@gmail.com>
2025-05-18 07:10:00 +0900

    implement FMA16 function and add tests and benchmarks for fused multiply-add

--- utils.go
+++ utils.go
@@ -2,13 +2,32 @@ package floats
 
 import "github.com/shogo82148/ints"
 
-// squash16 squashes the bits of x to a single bit.
-func squash16(x uint16) uint16 {
-	x |= x >> 8
-	x |= x >> 4
-	x |= x >> 2
-	x |= x >> 1
-	return x & 1
+func zero16(x uint16) uint16 {
+	if x == 0 {
+		return 1
+	}
+	return 0
+}
+
+func nonzero16(x uint16) uint16 {
+	if x != 0 {
+		return 1
+	}
+	return 0
+}
+
+func zero32(x uint32) uint32 {
+	if x == 0 {
+		return 1
+	}
+	return 0
+}
+
+func nonzero32(x uint32) uint32 {
+	if x != 0 {
+		return 1
+	}
+	return 0
 }
 
 // squash64 squashes the bits of x to a single bit.
@@ -44,6 +63,30 @@ func squash512(x ints.Uint512) uint64 {
 	y |= y >> 2
 	y |= y >> 1
 	return y & 1
+}
+
+func shrcompress16(x uint16, n uint) uint16 {
+	if n >= 16 {
+		return nonzero16(x)
+	}
+	y := x >> n
+	y |= nonzero16(x & (1<<n - 1))
+	return y
+}
+
+func shrcompress32(x uint32, n uint) uint32 {
+	if n >= 32 {
+		return nonzero32(x)
+	}
+	y := x >> n
+	y |= nonzero32(x & (1<<n - 1))
+	return y
+}
+
+func roundToNearestEven16(x uint16, shift uint) uint16 {
+	mask := uint16(1)<<(shift-1) - 1
+	x = (x + mask) + ((x >> shift) & 1)
+	return x >> shift
 }
 
 func roundToNearestEven128(x ints.Uint128, shift uint) ints.Uint128 {
