215233adc8891ea6c22c36963480d528a49ae99f
ICHINOSE Shogo <shogo82148@gmail.com>
2025-12-28 17:14:32 +0900

    implement scientific notation formatting for Float16 in formatDigits function

--- ftoa.go
+++ ftoa.go
@@ -3,6 +3,7 @@ package floats
 func formatDigits(dst []byte, neg bool, d *decimal, shortest bool, prec int, fmt byte) []byte {
 	switch fmt {
 	case 'e', 'E':
+		return fmtE(dst, neg, d, prec, fmt)
 	case 'f':
 		return fmtF(dst, neg, d, prec)
 	case 'g', 'G':
@@ -10,6 +11,61 @@ func formatDigits(dst []byte, neg bool, d *decimal, shortest bool, prec int, fmt
 
 	// unknown format
 	return append(dst, '%', fmt)
+}
+
+// %e: -d.ddddde±dd
+func fmtE(dst []byte, neg bool, d *decimal, prec int, fmt byte) []byte {
+	// sign
+	if neg {
+		dst = append(dst, '-')
+	}
+
+	// first digit
+	ch := byte('0')
+	if d.nd != 0 {
+		ch = d.d[0]
+	}
+	dst = append(dst, ch)
+
+	// .moredigits
+	if prec > 0 {
+		dst = append(dst, '.')
+		i := 1
+		m := min(d.nd, prec+1)
+		if i < m {
+			dst = append(dst, d.d[i:m]...)
+			i = m
+		}
+		for ; i <= prec; i++ {
+			dst = append(dst, '0')
+		}
+	}
+
+	// e±
+	dst = append(dst, fmt)
+	exp := d.dp - 1
+	if d.nd == 0 { // special case: 0 has exponent 0
+		exp = 0
+	}
+	if exp < 0 {
+		ch = '-'
+		exp = -exp
+	} else {
+		ch = '+'
+	}
+	dst = append(dst, ch)
+
+	// dd or ddd
+	switch {
+	case exp < 10:
+		dst = append(dst, '0', byte(exp)+'0')
+	case exp < 100:
+		dst = append(dst, byte(exp/10)+'0', byte(exp%10)+'0')
+	default:
+		dst = append(dst, byte(exp/100)+'0', byte(exp/10)%10+'0', byte(exp%10)+'0')
+	}
+
+	return dst
 }
 
 // %f: -ddddddd.ddddd
