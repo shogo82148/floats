0f0c6f9747c6b84b10d1001c065ad95fe32ef1d9
ICHINOSE Shogo <shogo82148@gmail.com>
2025-12-28 17:41:03 +0900

    implement Float32 type and its string representation methods

---
+++ f32toa.go
@@ -0,0 +1,23 @@
+package floats
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var _ fmt.Stringer = Float32(0)
+
+// String returns the string representation of a.
+func (a Float32) String() string {
+	return a.Text('g', -1)
+}
+
+// Text returns the string representation of a in the given format and precision.
+func (a Float32) Text(fmt byte, prec int) string {
+	return strconv.FormatFloat(float64(a), fmt, prec, 32)
+}
+
+// Append appends the string representation of a in the given format and precision to dst and returns the extended buffer.
+func (a Float32) Append(dst []byte, fmt byte, prec int) []byte {
+	return strconv.AppendFloat(dst, float64(a), fmt, prec, 32)
+}
