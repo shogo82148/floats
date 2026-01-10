f00bedd5d7fc48f53568c1f4deb64f0b750fce77
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-11 03:17:32 +0900

    implement Float128.Round and Float128.RoundToEven

--- floor128.go
+++ floor128.go
@@ -1,5 +1,7 @@
 package floats
 
+import "github.com/shogo82148/ints"
+
 // Floor returns the greatest integer value less than or equal to a.
 //
 // Special cases are:
@@ -48,4 +50,63 @@ func (a Float128) Trunc() Float128 {
 	}
 	d, _ := a.Modf()
 	return d
+}
+
+// Round returns the nearest integer, rounding half away from zero.
+//
+// Special cases are:
+//
+//	±0.Round() = ±0
+//	±Inf.Round() = ±Inf
+//	NaN.Round() = NaN
+func (a Float128) Round() Float128 {
+	bits := a.Bits()
+	e := uint((a[0] >> (shift128 - 64)) & mask128)
+	if e < bias128 {
+		// Round abs(x) < 1 including denormals.
+		bits = bits.And(signMask128) // ±0
+		if e == bias128-1 {
+			// abs(x) >= 0.5
+			bits = bits.Or(uvone128) // ±1
+		}
+	} else if e < bias128+shift128 {
+		// Round any abs(x) >= 1 containing a fractional component [0,1).
+		//
+		// Numbers with larger exponents are returned unchanged since they
+		// must be either an integer, infinity, or NaN.
+		half := ints.Uint128{1 << (shift128 - 64 - 1), 0}
+		e -= bias128
+		bits = bits.Add(half.Rsh(e))
+		bits = bits.AndNot(fracMask128.Rsh(e))
+	}
+	return NewFloat128FromBits(bits)
+}
+
+// RoundToEven returns the nearest integer, rounding ties to even.
+//
+// Special cases are:
+//
+//	±0.RoundToEven() = ±0
+//	±Inf.RoundToEven() = ±Inf
+//	NaN.RoundToEven() = NaN
+func (a Float128) RoundToEven() Float128 {
+	bits := a.Bits()
+	e := uint((a[0] >> (shift128 - 64)) & mask128)
+	if e >= bias128 {
+		// Round abs(x) >= 1.
+		// - Large numbers without fractional components, infinity, and NaN are unchanged.
+		// - Add 0.499.. or 0.5 before truncating depending on whether the truncated
+		//   number is even or odd (respectively).
+		halfMinusULP := ints.Uint128{1 << (shift128 - 64 - 1), 0}.Sub(ints.Uint128{0, 1})
+		e -= bias128
+		bits = bits.Add(halfMinusULP.Add(bits.Rsh(shift128 - e).And(ints.Uint128{0, 1})).Rsh(e))
+		bits = bits.AndNot(fracMask128.Rsh(e))
+	} else if e == bias128-1 && !bits.And(fracMask128).IsZero() {
+		// Round 0.5 < abs(x) < 1.
+		bits = bits.And(signMask128).Or(uvone128) // ±1
+	} else {
+		// Round abs(x) <= 0.5 including denormals.
+		bits = bits.And(signMask128) // ±0
+	}
+	return NewFloat128FromBits(bits)
 }
--- floor128_test.go
+++ floor128_test.go
@@ -82,3 +82,64 @@ func TestFloat128_Trunc(t *testing.T) {
 		}
 	}
 }
+
+func TestFloat128_Round(t *testing.T) {
+	tests := []struct {
+		x    Float128
+		want Float128
+	}{
+		{exact128(0.25), exact128(0)},
+		{exact128(0.5), exact128(1)},
+		{exact128(3.0), exact128(3)},
+		{exact128(3.5), exact128(4)},
+		{exact128(4.5), exact128(5)},
+		{exact128(-3.0), exact128(-3)},
+		{exact128(-3.5), exact128(-4)},
+		{exact128(-4.5), exact128(-5)},
+
+		// Special cases
+		{exact128(0), exact128(0)},
+		{exact128(math.Copysign(0, -1)), exact128(math.Copysign(0, -1))},
+		{exact128(math.Inf(1)), exact128(math.Inf(1))},
+		{exact128(math.Inf(-1)), exact128(math.Inf(-1))},
+		{exact128(math.NaN()), exact128(math.NaN())},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Round()
+		if !eq128(got, tt.want) {
+			t.Errorf("Float128.Round(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
+
+func TestFloat128_RoundToEven(t *testing.T) {
+	tests := []struct {
+		x    Float128
+		want Float128
+	}{
+		{exact128(0.25), exact128(0)},
+		{exact128(0.5), exact128(0)},
+		{exact128(0.75), exact128(1)},
+		{exact128(3.0), exact128(3)},
+		{exact128(3.5), exact128(4)},
+		{exact128(4.5), exact128(4)},
+		{exact128(-3.0), exact128(-3)},
+		{exact128(-3.5), exact128(-4)},
+		{exact128(-4.5), exact128(-4)},
+
+		// Special cases
+		{exact128(0), exact128(0)},
+		{exact128(math.Copysign(0, -1)), exact128(math.Copysign(0, -1))},
+		{exact128(math.Inf(1)), exact128(math.Inf(1))},
+		{exact128(math.Inf(-1)), exact128(math.Inf(-1))},
+		{exact128(math.NaN()), exact128(math.NaN())},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.RoundToEven()
+		if !eq128(got, tt.want) {
+			t.Errorf("Float128.RoundToEven(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
