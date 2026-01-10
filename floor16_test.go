d9df7d5d4b6a6789a59597c44519200a9832b808
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-11 02:01:22 +0900

    implement Float16.Trunc

--- floor16_test.go
+++ floor16_test.go
@@ -56,3 +56,29 @@ func TestFloat16_Ceil(t *testing.T) {
 		}
 	}
 }
+
+func TestFloat16_Trunc(t *testing.T) {
+	tests := []struct {
+		x    Float16
+		want Float16
+	}{
+		{exact16(3.0), exact16(3)},
+		{exact16(3.5), exact16(3)},
+		{exact16(-3.0), exact16(-3)},
+		{exact16(-3.5), exact16(-3)},
+
+		// Special cases
+		{exact16(0), exact16(0)},
+		{exact16(math.Copysign(0, -1)), exact16(math.Copysign(0, -1))},
+		{exact16(math.Inf(1)), exact16(math.Inf(1))},
+		{exact16(math.Inf(-1)), exact16(math.Inf(-1))},
+		{exact16(math.NaN()), exact16(math.NaN())},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Trunc()
+		if !eq16(got, tt.want) {
+			t.Errorf("Float16.Trunc(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
