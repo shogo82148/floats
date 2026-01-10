89942f718e6fdcb078e8d8c9ddd251ef87cf62b5
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-11 02:08:21 +0900

    implement Float256.Trunc

--- floor256_test.go
+++ floor256_test.go
@@ -56,3 +56,29 @@ func TestFloat256_Ceil(t *testing.T) {
 		}
 	}
 }
+
+func TestFloat256_Trunc(t *testing.T) {
+	tests := []struct {
+		x    Float256
+		want Float256
+	}{
+		{exact256(3.0), exact256(3)},
+		{exact256(3.5), exact256(3)},
+		{exact256(-3.0), exact256(-3)},
+		{exact256(-3.5), exact256(-3)},
+
+		// Special cases
+		{exact256(0), exact256(0)},
+		{exact256(math.Copysign(0, -1)), exact256(math.Copysign(0, -1))},
+		{exact256(math.Inf(1)), exact256(math.Inf(1))},
+		{exact256(math.Inf(-1)), exact256(math.Inf(-1))},
+		{exact256(math.NaN()), exact256(math.NaN())},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Trunc()
+		if !eq256(got, tt.want) {
+			t.Errorf("Float256.Trunc(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
