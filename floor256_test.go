62ecb43175e7bf046bb949d6faf8e8043fb914b0
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-11 01:26:46 +0900

    implement Float256.Ceil

--- floor256_test.go
+++ floor256_test.go
@@ -30,3 +30,29 @@ func TestFloat256_Floor(t *testing.T) {
 		}
 	}
 }
+
+func TestFloat256_Ceil(t *testing.T) {
+	tests := []struct {
+		x    Float256
+		want Float256
+	}{
+		{exact256(3.0), exact256(3)},
+		{exact256(3.5), exact256(4)},
+		{exact256(-3.0), exact256(-3)},
+		{exact256(-3.5), exact256(-3)},
+
+		// Special cases
+		{exact256(0), exact256(0)},
+		{exact256(math.Copysign(0, -1)), exact256(math.Copysign(0, -1))},
+		{exact256(math.Inf(1)), exact256(math.Inf(1))},
+		{exact256(math.Inf(-1)), exact256(math.Inf(-1))},
+		{exact256(math.NaN()), exact256(math.NaN())},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Ceil()
+		if !eq256(got, tt.want) {
+			t.Errorf("Float256.Floor(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
