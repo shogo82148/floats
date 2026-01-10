c3d522f7454a36e247e6639a8323b80ce0eba1cf
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-11 02:41:57 +0900

    implement Float256.Max and Float256.Min

--- dim256_test.go
+++ dim256_test.go
@@ -29,3 +29,57 @@ func TestFloat256_Dim(t *testing.T) {
 		}
 	}
 }
+
+func TestFloat256_Max(t *testing.T) {
+	tests := []struct {
+		a    Float256
+		b    Float256
+		want Float256
+	}{
+		{exact256(5.0), exact256(3.0), exact256(5.0)},
+		{exact256(3.0), exact256(5.0), exact256(5.0)},
+		{exact256(3.0), exact256(3.0), exact256(3.0)},
+
+		// Special cases
+		{exact256(1), exact256(math.Inf(1)), exact256(math.Inf(1))},
+		{exact256(math.Inf(1)), exact256(1), exact256(math.Inf(1))},
+		{exact256(math.NaN()), exact256(1), exact256(math.NaN())},
+		{exact256(1), exact256(math.NaN()), exact256(math.NaN())},
+		{exact256(0), exact256(math.Copysign(0, -1)), exact256(0)},
+		{exact256(math.Copysign(0, -1)), exact256(0), exact256(0)},
+		{exact256(math.Copysign(0, -1)), exact256(math.Copysign(0, -1)), exact256(math.Copysign(0, -1))},
+	}
+	for _, tt := range tests {
+		got := tt.a.Max(tt.b)
+		if !eq256(got, tt.want) {
+			t.Errorf("Float256.Max(%v, %v) = %v; want %v", tt.a, tt.b, got, tt.want)
+		}
+	}
+}
+
+func TestFloat256_Min(t *testing.T) {
+	tests := []struct {
+		a    Float256
+		b    Float256
+		want Float256
+	}{
+		{exact256(5.0), exact256(3.0), exact256(3.0)},
+		{exact256(3.0), exact256(5.0), exact256(3.0)},
+		{exact256(3.0), exact256(3.0), exact256(3.0)},
+
+		// Special cases
+		{exact256(1), exact256(math.Inf(-1)), exact256(math.Inf(-1))},
+		{exact256(math.Inf(-1)), exact256(1), exact256(math.Inf(-1))},
+		{exact256(math.NaN()), exact256(1), exact256(math.NaN())},
+		{exact256(1), exact256(math.NaN()), exact256(math.NaN())},
+		{exact256(0), exact256(math.Copysign(0, -1)), exact256(math.Copysign(0, -1))},
+		{exact256(math.Copysign(0, -1)), exact256(0), exact256(math.Copysign(0, -1))},
+		{exact256(math.Copysign(0, -1)), exact256(math.Copysign(0, -1)), exact256(math.Copysign(0, -1))},
+	}
+	for _, tt := range tests {
+		got := tt.a.Min(tt.b)
+		if !eq256(got, tt.want) {
+			t.Errorf("Float256.Min(%v, %v) = %v; want %v", tt.a, tt.b, got, tt.want)
+		}
+	}
+}
