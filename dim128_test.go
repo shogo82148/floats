2f09846a9c2bcc6921f8f7dd2c52ae44c30e24da
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-11 01:55:47 +0900

    implement Float128.Dim

---
+++ dim128_test.go
@@ -0,0 +1,31 @@
+package floats
+
+import (
+	"math"
+	"testing"
+)
+
+func TestFloat128_Dim(t *testing.T) {
+	tests := []struct {
+		a    Float128
+		b    Float128
+		want Float128
+	}{
+		{exact128(5.0), exact128(3.0), exact128(2.0)},
+		{exact128(3.0), exact128(5.0), exact128(0.0)},
+		{exact128(3.0), exact128(3.0), exact128(0.0)},
+
+		// Special cases
+		{exact128(math.Inf(1)), exact128(math.Inf(1)), exact128(math.NaN())},
+		{exact128(math.Inf(-1)), exact128(math.Inf(-1)), exact128(math.NaN())},
+		{exact128(math.NaN()), exact128(1.0), exact128(math.NaN())},
+		{exact128(1.0), exact128(math.NaN()), exact128(math.NaN())},
+	}
+
+	for _, tt := range tests {
+		got := tt.a.Dim(tt.b)
+		if !eq128(got, tt.want) {
+			t.Errorf("Float128.Dim(%v, %v) = %v; want %v", tt.a, tt.b, got, tt.want)
+		}
+	}
+}
