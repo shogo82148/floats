afbd03de91e0cd2eb010e120367b0cebe6814cfa
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-11 13:48:27 +0900

    implement Float64.Logb and Float64.Ilogb

---
+++ logb64_test.go
@@ -0,0 +1,71 @@
+package floats
+
+import (
+	"math"
+	"runtime"
+	"testing"
+)
+
+func TestFloat64_Logb(t *testing.T) {
+	tests := []struct {
+		x    Float64
+		want Float64
+	}{
+		{exact64(1), exact64(0)},
+		{exact64(2), exact64(1)},
+		{exact64(0.5), exact64(-1)},
+		{exact64(4), exact64(2)},
+		{exact64(0.25), exact64(-2)},
+
+		// special values
+		{exact64(0), exact64(math.Inf(-1))},
+		{exact64(math.Inf(1)), exact64(math.Inf(1))},
+		{exact64(math.NaN()), exact64(math.NaN())},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Logb()
+		if !eq64(got, tt.want) {
+			t.Errorf("Logb(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
+
+func BenchmarkFloat64_Logb(b *testing.B) {
+	x := exact64(1)
+	for b.Loop() {
+		runtime.KeepAlive(x.Logb())
+	}
+}
+
+func TestFloat64_Ilogb(t *testing.T) {
+	tests := []struct {
+		x    Float64
+		want int
+	}{
+		{exact64(1), 0},
+		{exact64(2), 1},
+		{exact64(0.5), -1},
+		{exact64(4), 2},
+		{exact64(0.25), -2},
+
+		// special values
+		{exact64(0), math.MinInt32},
+		{exact64(math.Inf(1)), math.MaxInt32},
+		{exact64(math.NaN()), math.MaxInt32},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Ilogb()
+		if got != tt.want {
+			t.Errorf("Ilogb(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
+
+func BenchmarkFloat64_Ilogb(b *testing.B) {
+	x := exact64(1)
+	for b.Loop() {
+		runtime.KeepAlive(x.Ilogb())
+	}
+}
