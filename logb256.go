f17e7c2018f0b82ba94f16097dda7a581edea3ab
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-11 13:52:20 +0900

    implement Float256.Logb and Float256.Ilogb

---
+++ logb256.go
@@ -0,0 +1,45 @@
+package floats
+
+import "math"
+
+// Logb returns the binary exponent of a.
+//
+// Special cases are:
+//
+//	±Inf.Logb() = +Inf
+//	0.Logb() = -Inf
+//	NaN.Logb() = NaN
+func (a Float256) Logb() Float256 {
+	// special cases
+	switch {
+	case a.IsZero():
+		return NewFloat256Inf(-1)
+	case a.IsInf(0):
+		return NewFloat256Inf(1)
+	case a.IsNaN():
+		return NewFloat256NaN()
+	}
+	_, exp, _ := a.normalize()
+	return NewFloat256(float64(exp))
+}
+
+// Ilogb returns the binary exponent of x as an integer.
+//
+// Special cases are:
+//
+//	±Inf.Ilogb() = MaxInt32
+//	0.Ilogb() = MinInt32
+//	NaN.Ilogb() = MaxInt32
+func (a Float256) Ilogb() int {
+	// special cases
+	switch {
+	case a.IsZero():
+		return math.MinInt32
+	case a.IsInf(0):
+		return math.MaxInt32
+	case a.IsNaN():
+		return math.MaxInt32
+	}
+	_, exp, _ := a.normalize()
+	return exp
+}
