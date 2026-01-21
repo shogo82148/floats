2b28a1c4ebb494187aca19e071f7716a6fec4550
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-21 17:07:25 +0900

    implement Float32.Cosh

--- sinh32_test.go
+++ sinh32_test.go
@@ -60,3 +60,50 @@ func BenchmarkFloat32_Sinh(b *testing.B) {
 		runtime.KeepAlive(x.Sinh())
 	}
 }
+
+func TestFloat32_Cosh(t *testing.T) {
+	tests := []struct {
+		x    Float32
+		want float64
+	}{
+		{exact32(0), math.Cosh(0)},
+		{exact32(1), math.Cosh(1)},
+		{exact32(2), math.Cosh(2)},
+		{exact32(3), math.Cosh(3)},
+		{exact32(4), math.Cosh(4)},
+		{exact32(22), math.Cosh(22)},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Cosh()
+		if !close32(got, tt.want) {
+			t.Errorf("Cosh(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+
+	strictTests := []struct {
+		x    Float32
+		want Float32
+	}{
+		// special cases
+		{exact32(0), exact32(1)},
+		{exact32(math.Copysign(0, -1)), exact32(1)},
+		{exact32(math.Inf(1)), exact32(math.Inf(1))},
+		{exact32(math.Inf(-1)), exact32(math.Inf(1))},
+		{exact32(math.NaN()), exact32(math.NaN())},
+	}
+
+	for _, tt := range strictTests {
+		got := tt.x.Cosh()
+		if !eq32(got, tt.want) {
+			t.Errorf("Cosh(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
+
+func BenchmarkFloat32_Cosh(b *testing.B) {
+	x := exact32(1.5)
+	for b.Loop() {
+		runtime.KeepAlive(x.Cosh())
+	}
+}
