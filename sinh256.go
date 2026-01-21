9ad2e9a6904d405e42f931e2d1f895284d2cf499
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-21 17:40:40 +0900

    implement Float256.Cosh

--- sinh256.go
+++ sinh256.go
@@ -41,7 +41,7 @@ func (a Float256) Sinh() Float256 {
 
 	case a.Gt(Half):
 		ex := a.Exp()
-		temp = (ex.Sub(One.Quo(ex))).Mul(Half)
+		temp = ex.Sub(One.Quo(ex)).Mul(Half)
 
 	default:
 		// Taylor series expansion
@@ -55,4 +55,39 @@ func (a Float256) Sinh() Float256 {
 		temp = temp.Neg()
 	}
 	return temp
+}
+
+// Cosh returns the hyperbolic cosine of x.
+//
+// Special cases are:
+//
+//	±0.Cosh() = 1
+//	±Inf.Cosh() = +Inf
+//	NaN.Cosh() = NaN
+func (a Float256) Cosh() Float256 {
+	var (
+		// Large = 84
+		Large = Float256{
+			0x4000_5500_0000_0000, 0x0000_0000_0000_0000,
+			0x0000_0000_0000_0000, 0x0000_0000_0000_0000,
+		}
+
+		// One = 1.0
+		One = Float256{
+			0x3fff_f000_0000_0000, 0x0000_0000_0000_0000,
+			0x0000_0000_0000_0000, 0x0000_0000_0000_0000,
+		}
+
+		// Half = 0.5
+		Half = Float256{
+			0x3fff_e000_0000_0000, 0x0000_0000_0000_0000,
+			0x0000_0000_0000_0000, 0x0000_0000_0000_0000,
+		}
+	)
+	a = a.Abs()
+	if a.Gt(Large) {
+		return a.Exp().Mul(Half)
+	}
+	ex := a.Exp()
+	return ex.Add(One.Quo(ex)).Mul(Half)
 }
