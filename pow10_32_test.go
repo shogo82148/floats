f65d1b3e6c351aae8153c4a82da6ca430e0b7342
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-24 23:32:32 +0900

    refactor: improve documentation and tests for NewFloat32Pow10

--- pow10_32_test.go
+++ pow10_32_test.go
@@ -1,6 +1,9 @@
 package floats
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestNewFloat32Pow10(t *testing.T) {
 	tests := []struct {
@@ -12,6 +15,7 @@ func TestNewFloat32Pow10(t *testing.T) {
 		{2, 100},
 		{3, 1000},
 		{4, 10000},
+		{38, 1e38},
 		{-1, 0.1},
 		{-2, 0.01},
 		{-3, 0.001},
@@ -22,6 +26,21 @@ func TestNewFloat32Pow10(t *testing.T) {
 		got := NewFloat32Pow10(test.n)
 		if !close32(got, test.want) {
 			t.Errorf("NewFloat32Pow10(%d) = %v; want %v", test.n, got, test.want)
+		}
+	}
+
+	strictTests := []struct {
+		n    int
+		want Float32
+	}{
+		{39, exact32(math.Inf(1))}, // overflow
+		{-46, exact32(0)},          // underflow
+	}
+
+	for _, tt := range strictTests {
+		got := NewFloat32Pow10(tt.n)
+		if !eq32(got, tt.want) {
+			t.Errorf("NewFloat32Pow10(%d) = %v; want %v", tt.n, got, tt.want)
 		}
 	}
 }
