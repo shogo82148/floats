8fe6779f9355bbfb7dd97111fb195d93f0367b7f
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-25 01:31:18 +0900

    refactor Log1p documentation to use consistent argument naming

--- log1p16.go
+++ log1p16.go
@@ -2,15 +2,15 @@ package floats
 
 import "math"
 
-// Log1p returns the natural logarithm of 1 plus its argument x.
-// It is more accurate than [Log](1 + x) when x is near zero.
+// Log1p returns the natural logarithm of 1 plus its argument a.
+// It is more accurate than [Log](1 + a) when a is near zero.
 //
 // Special cases are:
 //
 //	+Inf.Log1p() = +Inf
 //	±0.Log1p() = ±0
 //	-1.Log1p() = -Inf
-//	(x < -1).Log1p() = NaN
+//	(a < -1).Log1p() = NaN
 //	NaN.Log1p() = NaN
 func (a Float16) Log1p() Float16 {
 	return NewFloat16(math.Log1p(a.Float64().BuiltIn()))
--- log1p32.go
+++ log1p32.go
@@ -1,14 +1,14 @@
 package floats
 
-// Log1p returns the natural logarithm of 1 plus its argument x.
-// It is more accurate than [Log](1 + x) when x is near zero.
+// Log1p returns the natural logarithm of 1 plus its argument a.
+// It is more accurate than [Log](1 + a) when a is near zero.
 //
 // Special cases are:
 //
 //	+Inf.Log1p() = +Inf
 //	±0.Log1p() = ±0
 //	-1.Log1p() = -Inf
-//	(x < -1).Log1p() = NaN
+//	(a < -1).Log1p() = NaN
 //	NaN.Log1p() = NaN
 func (a Float32) Log1p() Float32 {
 	const (
