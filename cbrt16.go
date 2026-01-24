c1251eafa73e703bf50689d83ea1c114cba4c1d5
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-24 21:22:39 +0900

    fix: update documentation for Float16.Cbrt to clarify input parameter

--- cbrt16.go
+++ cbrt16.go
@@ -2,13 +2,13 @@ package floats
 
 import "math"
 
-// Cbrt returns the cube root of x.
+// Cbrt returns the cube root of a.
 //
 // Special cases are:
 //
-//	Cbrt(±0) = ±0
-//	Cbrt(±Inf) = ±Inf
-//	Cbrt(NaN) = NaN
+//	±0.Cbrt() = ±0
+//	±Inf.Cbrt() = ±Inf
+//	NaN.Cbrt() = NaN
 func (a Float16) Cbrt() Float16 {
 	return NewFloat16(math.Cbrt(a.Float64().BuiltIn()))
 }
