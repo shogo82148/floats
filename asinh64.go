7d7c493381786d2240a0b56dbe86166f08b517f7
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-25 07:46:05 +0900

    refactor Asinh documentation to use consistent parameter naming

--- asinh64.go
+++ asinh64.go
@@ -2,13 +2,13 @@ package floats
 
 import "math"
 
-// Asinh returns the inverse hyperbolic sine of x.
+// Asinh returns the inverse hyperbolic sine of a.
 //
 // Special cases are:
 //
-//	Asinh(±0) = ±0
-//	Asinh(±Inf) = ±Inf
-//	Asinh(NaN) = NaN
+//	±0.Asinh() = ±0
+//	±Inf.Asinh() = ±Inf
+//	NaN.Asinh() = NaN
 func (a Float64) Asinh() Float64 {
 	return NewFloat64(math.Asinh(a.BuiltIn()))
 }
