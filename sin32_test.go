f75ddb1af601970d713e1476d6cdcbefeba388dc
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-25 22:58:30 +0900

    implement Float32.Sincos

--- sin32_test.go
+++ sin32_test.go
@@ -78,3 +78,48 @@ func TestFloat32_Cos(t *testing.T) {
 		}
 	}
 }
+
+func TestFloat32_Sincos(t *testing.T) {
+	tests := []struct {
+		x   Float32
+		sin float64
+		cos float64
+	}{
+		{exact32(1), math.Sin(1), math.Cos(1)},
+		{exact32(2), math.Sin(2), math.Cos(2)},
+		{exact32(3), math.Sin(3), math.Cos(3)},
+	}
+
+	for _, tt := range tests {
+		sin, cos := tt.x.Sincos()
+		if !close32(sin, tt.sin) {
+			t.Errorf("Sincos(%v) sin = %v; want %v", tt.x, sin, tt.sin)
+		}
+		if !close32(cos, tt.cos) {
+			t.Errorf("Sincos(%v) cos = %v; want %v", tt.x, cos, tt.cos)
+		}
+	}
+
+	strictTests := []struct {
+		x   Float32
+		sin Float32
+		cos Float32
+	}{
+		// special cases
+		{exact32(0), exact32(0), exact32(1)},
+		{exact32(math.Copysign(0, -1)), exact32(math.Copysign(0, -1)), exact32(1)},
+		{exact32(math.Inf(1)), exact32(math.NaN()), exact32(math.NaN())},
+		{exact32(math.Inf(-1)), exact32(math.NaN()), exact32(math.NaN())},
+		{exact32(math.NaN()), exact32(math.NaN()), exact32(math.NaN())},
+	}
+
+	for _, tt := range strictTests {
+		sin, cos := tt.x.Sincos()
+		if !eq32(sin, tt.sin) {
+			t.Errorf("Sincos(%v) sin = %v; want %v", tt.x, sin, tt.sin)
+		}
+		if !eq32(cos, tt.cos) {
+			t.Errorf("Sincos(%v) cos = %v; want %v", tt.x, cos, tt.cos)
+		}
+	}
+}
