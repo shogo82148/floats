74f3e8b5a987af4c569909fdb8fdaeb075290873
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-25 23:12:11 +0900

    implement Float256.Sincos

--- sin256.go
+++ sin256.go
@@ -193,3 +193,109 @@ func (a Float256) Cos() Float256 {
 	}
 	return y
 }
+
+// Sincos returns Sin(a), Cos(a).
+//
+// Special cases are:
+//
+//	±0.Sincos() = ±0, 1
+//	±Inf.Sincos() = NaN, NaN
+//	NaN.Sincos() = NaN, NaN
+func (a Float256) Sincos() (sin, cos Float256) {
+	var (
+		Zero = Float256{}
+		One  = Float256(uvone256)
+
+		// MPI4 = 4/pi
+		MPI4 = Float256{
+			0x3fff_f45f_306d_c9c8, 0x82a5_3f84_eafa_3ea6,
+			0x9bb8_1b6c_52b3_2788, 0x7208_3fca_2c75_7bd7,
+		}
+
+		// Pi/4 split into three parts
+		PI4A = Float256{
+			0x3fff_e921_fb54_442d, 0x1846_9898_cc51_701b, 0x8300_0000_0000_0000, 0x0000_0000_0000_0000,
+		}
+		PI4B = Float256{
+			0x3ff8_9344_a409_3822, 0x299f_31d0_082e_fa98,
+			0xec00_0000_0000_0000, 0x0000_0000_0000_0000,
+		}
+		PI4C = Float256{
+			0x3ff1_339b_2251_4a08, 0x798e_3404_ddef_9519,
+			0xb3cd_3a43_1b30_2b0a, 0x6df2_5f79_0a22_9257,
+		}
+	)
+
+	// special cases
+	switch {
+	case a.IsZero():
+		return a, One // return ±0.0, 1.0
+	case a.IsNaN() || a.IsInf(0):
+		return NewFloat256NaN(), NewFloat256NaN()
+	}
+
+	// make argument positive
+	sinSign, cosSign := false, false
+	if a.Lt(Zero) {
+		a = a.Neg()
+		sinSign = true
+	}
+
+	var j uint64
+	var y, z Float256
+	// TODO: use Payne-Hanek reduction for large arguments
+	j = a.Mul(MPI4).Uint64()
+	y = NewFloat256(float64(j))
+
+	// map zeros to origin
+	if j&1 == 1 {
+		j++
+		y = y.Add(One)
+	}
+	j &= 7 // octant modulo 2Pi radians (360 degrees)
+
+	// Extended precision modular arithmetic
+	y = y.Neg()
+	z = FMA256(y, PI4A, a)
+	z = FMA256(y, PI4B, z)
+	z = FMA256(y, PI4C, z)
+
+	if j > 3 { // reflect in x axis
+		j -= 4
+		sinSign, cosSign = !sinSign, !cosSign
+	}
+	if j > 1 {
+		cosSign = !cosSign
+	}
+
+	// taylor series expansion of sin around 0
+	sin = Zero
+	for n := 50; n >= 0; n-- {
+		term := power256(z, 2*n+1).Quo(factorial256(2*n + 1))
+		if n%2 != 0 {
+			term = term.Neg()
+		}
+		sin = sin.Add(term)
+	}
+
+	// taylor series expansion of cos around 0
+	cos = Zero
+	for n := 50; n >= 0; n-- {
+		term := power256(z, 2*n).Quo(factorial256(2 * n))
+		if n%2 != 0 {
+			term = term.Neg()
+		}
+		cos = cos.Add(term)
+	}
+
+	if j == 1 || j == 2 {
+		sin, cos = cos, sin
+	}
+	if cosSign {
+		cos = cos.Neg()
+	}
+	if sinSign {
+		sin = sin.Neg()
+	}
+	return
+}
--- sin256_test.go
+++ sin256_test.go
@@ -82,3 +82,60 @@ func TestFloat256_Cos(t *testing.T) {
 		}
 	}
 }
+
+func TestFloat256_Sincos(t *testing.T) {
+	tests := []struct {
+		x   Float256
+		sin string
+		cos string
+	}{
+		{
+			exact256(1),
+			"0.841470984807896506652502321630298999622563060798371065672751709991910404391239669",
+			"0.5403023058681397174009366074429766037323104206179222276700972553811003947744717645",
+		},
+		{
+			exact256(2),
+			"0.9092974268256816953960198659117448427022549714478902683789730115309673015407835446",
+			"-0.4161468365471423869975682295007621897660007710755448907551499737819649361240791691",
+		},
+		{
+			exact256(3),
+			"0.1411200080598672221007448028081102798469332642522655841518826412324220099670144719",
+			"-0.9899924966004454572715727947312613023936790966155883288140859329283291975131332204",
+		},
+	}
+
+	for _, tt := range tests {
+		sin, cos := tt.x.Sincos()
+		if !close256(sin, tt.sin) {
+			t.Errorf("Sincos(%v) sin = %v; want %v", tt.x, sin, tt.sin)
+		}
+		if !close256(cos, tt.cos) {
+			t.Errorf("Sincos(%v) cos = %v; want %v", tt.x, cos, tt.cos)
+		}
+	}
+
+	strictTests := []struct {
+		x   Float256
+		sin Float256
+		cos Float256
+	}{
+		// special cases
+		{exact256(0), exact256(0), exact256(1)},
+		{exact256(math.Copysign(0, -1)), exact256(math.Copysign(0, -1)), exact256(1)},
+		{exact256(math.Inf(1)), exact256(math.NaN()), exact256(math.NaN())},
+		{exact256(math.Inf(-1)), exact256(math.NaN()), exact256(math.NaN())},
+		{exact256(math.NaN()), exact256(math.NaN()), exact256(math.NaN())},
+	}
+
+	for _, tt := range strictTests {
+		sin, cos := tt.x.Sincos()
+		if !eq256(sin, tt.sin) {
+			t.Errorf("Sincos(%v) sin = %v; want %v", tt.x, sin, tt.sin)
+		}
+		if !eq256(cos, tt.cos) {
+			t.Errorf("Sincos(%v) cos = %v; want %v", tt.x, cos, tt.cos)
+		}
+	}
+}
