75b77cebfa88223f0dcd8d930b986db601f3b2b2
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-25 22:56:54 +0900

    implement Float16.Sincos

--- sin16.go
+++ sin16.go
@@ -22,3 +22,15 @@ func (a Float16) Sin() Float16 {
 func (a Float16) Cos() Float16 {
 	return NewFloat16(math.Cos(a.Float64().BuiltIn()))
 }
+
+// Sincos returns Sin(a), Cos(a).
+//
+// Special cases are:
+//
+//	±0.Sincos() = ±0, 1
+//	±Inf.Sincos() = NaN, NaN
+//	NaN.Sincos() = NaN, NaN
+func (a Float16) Sincos() (sin, cos Float16) {
+	s, c := math.Sincos(a.Float64().BuiltIn())
+	return NewFloat16(s), NewFloat16(c)
+}
--- sin16_test.go
+++ sin16_test.go
@@ -74,3 +74,48 @@ func TestFloat16_Cos(t *testing.T) {
 		}
 	}
 }
+
+func TestFloat16_Sincos(t *testing.T) {
+	tests := []struct {
+		x   Float16
+		sin float64
+		cos float64
+	}{
+		{exact16(1), math.Sin(1), math.Cos(1)},
+		{exact16(2), math.Sin(2), math.Cos(2)},
+		{exact16(3), math.Sin(3), math.Cos(3)},
+	}
+
+	for _, tt := range tests {
+		sin, cos := tt.x.Sincos()
+		if !close16(sin, tt.sin) {
+			t.Errorf("Sincos(%v) sin = %v; want %v", tt.x, sin, tt.sin)
+		}
+		if !close16(cos, tt.cos) {
+			t.Errorf("Sincos(%v) cos = %v; want %v", tt.x, cos, tt.cos)
+		}
+	}
+
+	strictTests := []struct {
+		x   Float16
+		sin Float16
+		cos Float16
+	}{
+		// special cases
+		{exact16(0), exact16(0), exact16(1)},
+		{exact16(math.Copysign(0, -1)), exact16(math.Copysign(0, -1)), exact16(1)},
+		{exact16(math.Inf(1)), exact16(math.NaN()), exact16(math.NaN())},
+		{exact16(math.Inf(-1)), exact16(math.NaN()), exact16(math.NaN())},
+		{exact16(math.NaN()), exact16(math.NaN()), exact16(math.NaN())},
+	}
+
+	for _, tt := range strictTests {
+		sin, cos := tt.x.Sincos()
+		if !eq16(sin, tt.sin) {
+			t.Errorf("Sincos(%v) sin = %v; want %v", tt.x, sin, tt.sin)
+		}
+		if !eq16(cos, tt.cos) {
+			t.Errorf("Sincos(%v) cos = %v; want %v", tt.x, cos, tt.cos)
+		}
+	}
+}
