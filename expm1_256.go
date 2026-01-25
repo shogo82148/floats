07d24030b2f909d2c63d5bd99be18ae8136a4349
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-25 15:12:25 +0900

    implement Float256.Expm1

--- expm1_256.go
+++ expm1_256.go
@@ -10,13 +10,16 @@ package floats
 //	NaN.Expm1() = NaN
 //
 // Very large values overflow to -1 or +Inf.
-func (a Float128) Expm1() Float128 {
+func (a Float256) Expm1() Float256 {
 	var (
-		// VerySmall = 2**-113
-		VerySmall = Float128{0x3f8e_0000_0000_0000, 0x0000_0000_0000_0000}
+		// VerySmall = 2**-237
+		VerySmall = Float256{0x3f8e_0000_0000_0000, 0x0000_0000_0000_0000}
 
 		// Small = 0.5
-		Small = Float128{0x3ffe_0000_0000_0000, 0x0000_0000_0000_0000}
+		Small = Float256{
+			0x3fff_e000_0000_0000, 0x0000_0000_0000_0000,
+			0x0000_0000_0000_0000, 0x0000_0000_0000_0000,
+		}
 	)
 	absa := a.Abs()
 	switch {
@@ -27,12 +30,12 @@ func (a Float128) Expm1() Float128 {
 		// use Taylor expansion when |x| is small
 		// exm1(x) = x + x**2/2! + x**3/3! + x**4/4! + x**5/5! + ...
 		// TODO: optimize using minimax approximation
-		var y Float128
-		for n := 20; n >= 1; n-- {
-			y = y.Add(power128(a, n).Quo(factorial128(n)))
+		var y Float256
+		for n := 40; n >= 1; n-- {
+			y = y.Add(power256(a, n).Quo(factorial256(n)))
 		}
 		return y
 	}
 
-	return a.Exp().Sub(Float128(uvone128))
+	return a.Exp().Sub(Float256(uvone256))
 }
