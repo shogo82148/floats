f27e0060f58c856c5bec10b0b3680f2f1c95a5b5
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-25 22:07:11 +0900

    implement Float16.Cos

--- sin16_test.go
+++ sin16_test.go
@@ -40,3 +40,37 @@ func TestFloat16_Sin(t *testing.T) {
 		}
 	}
 }
+
+func TestFloat16_Cos(t *testing.T) {
+	tests := []struct {
+		x    Float16
+		want float64
+	}{
+		{exact16(1), math.Cos(1)},
+		{exact16(2), math.Cos(2)},
+		{exact16(3), math.Cos(3)},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Cos()
+		if !close16(got, tt.want) {
+			t.Errorf("Cos(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+
+	strictTests := []struct {
+		x    Float16
+		want Float16
+	}{
+		// special cases
+		{exact16(math.Inf(1)), exact16(math.NaN())},
+		{exact16(math.Inf(-1)), exact16(math.NaN())},
+		{exact16(math.NaN()), exact16(math.NaN())},
+	}
+	for _, tt := range strictTests {
+		got := tt.x.Cos()
+		if !eq16(got, tt.want) {
+			t.Errorf("Cos(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
