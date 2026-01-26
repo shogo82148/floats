2a9248311734e12ba351eb8a7ae61737daf26f57
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-26 22:40:15 +0900

    implement Float64.Asin

--- asin64_test.go
+++ asin64_test.go
@@ -5,6 +5,48 @@ import (
 	"testing"
 )
 
+func TestFloat64_Asin(t *testing.T) {
+	tests := []struct {
+		x    Float64
+		want float64
+	}{
+		{exact64(-1), math.Asin(-1)},
+		{exact64(-0.75), math.Asin(-0.75)},
+		{exact64(-0.5), math.Asin(-0.5)},
+		{exact64(-0.25), math.Asin(-0.25)},
+		{exact64(0.25), math.Asin(0.25)},
+		{exact64(0.5), math.Asin(0.5)},
+		{exact64(0.75), math.Asin(0.75)},
+		{exact64(1), math.Asin(1)},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Asin()
+		if !close64(got, tt.want) {
+			t.Errorf("Asin(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+
+	strictTests := []struct {
+		x    Float64
+		want Float64
+	}{
+		// special cases
+		{exact64(0), exact64(0)},
+		{exact64(math.Copysign(0, -1)), exact64(math.Copysign(0, -1))},
+		{exact64(math.NaN()), exact64(math.NaN())},
+		{exact64(2), exact64(math.NaN())},
+		{exact64(-2), exact64(math.NaN())},
+	}
+
+	for _, tt := range strictTests {
+		got := tt.x.Asin()
+		if !eq64(got, tt.want) {
+			t.Errorf("Asin(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
+
 func TestFloat64_Atan(t *testing.T) {
 	tests := []struct {
 		x    Float64
