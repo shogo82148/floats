4b4efd56aa3b462dce1b80d69f44fe2a90c3a943
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-27 05:39:12 +0900

    implement Float32.Erf

---
+++ erf32_test.go
@@ -0,0 +1,52 @@
+package floats
+
+import (
+	"math"
+	"runtime"
+	"testing"
+)
+
+func TestFloat32_Erf(t *testing.T) {
+	tests := []struct {
+		x    Float32
+		want float64
+	}{
+		{exact32(0), math.Erf(0)},
+		{exact32(1), math.Erf(1)},
+		{exact32(2), math.Erf(2)},
+		{exact32(3), math.Erf(3)},
+		{exact32(4), math.Erf(4)},
+		{exact32(11), math.Erf(11)},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Erf()
+		if !close32(got, tt.want) {
+			t.Errorf("Erf(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+
+	strictTests := []struct {
+		x    Float32
+		want Float32
+	}{
+		// special cases
+		{exact32(math.Inf(1)), exact32(1)},
+		{exact32(math.Inf(-1)), exact32(-1)},
+		{exact32(math.NaN()), exact32(math.NaN())},
+	}
+
+	for _, tt := range strictTests {
+		got := tt.x.Erf()
+		if !eq32(got, tt.want) {
+			t.Errorf("Erf(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
+
+func BenchmarkFloat32_Erf(b *testing.B) {
+	x := exact32(1.5)
+	for b.Loop() {
+		runtime.KeepAlive(x.Erf())
+	}
+}
