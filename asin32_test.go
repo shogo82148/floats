f1fb5f35f6724ab758ebed28c3beeccc24b80818
ICHINOSE Shogo <shogo82148@gmail.com>
2026-01-26 21:22:51 +0900

    implement Float32.Atan

---
+++ asin32_test.go
@@ -0,0 +1,49 @@
+package floats
+
+import (
+	"math"
+	"testing"
+)
+
+func TestFloat32_Atan(t *testing.T) {
+	tests := []struct {
+		x    Float32
+		want float64
+	}{
+		{exact32(-0.5), math.Atan(-0.5)},
+		{exact32(-0.25), math.Atan(-0.25)},
+		{exact32(-0.125), math.Atan(-0.125)},
+		{exact32(0.125), math.Atan(0.125)},
+		{exact32(0.25), math.Atan(0.25)},
+		{exact32(0.5), math.Atan(0.5)},
+		{exact32(0.75), math.Atan(0.75)},
+		{exact32(1), math.Atan(1)},
+		{exact32(2), math.Atan(2)},
+		{exact32(math.Inf(-1)), math.Atan(math.Inf(-1))},
+		{exact32(math.Inf(1)), math.Atan(math.Inf(1))},
+	}
+
+	for _, tt := range tests {
+		got := tt.x.Atan()
+		if !close32(got, tt.want) {
+			t.Errorf("Atan(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+
+	strictTests := []struct {
+		x    Float32
+		want Float32
+	}{
+		// special cases
+		{exact32(0), exact32(0)},
+		{exact32(math.Copysign(0, -1)), exact32(math.Copysign(0, -1))},
+		{exact32(math.NaN()), exact32(math.NaN())},
+	}
+
+	for _, tt := range strictTests {
+		got := tt.x.Atan()
+		if !eq32(got, tt.want) {
+			t.Errorf("Atan(%v) = %v; want %v", tt.x, got, tt.want)
+		}
+	}
+}
